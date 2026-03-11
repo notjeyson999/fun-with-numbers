906f57d8a41436f15b00c60e8000b74dc205dcbb
notjeyson999 <jeyson.blain625@education.nsw.gov.au>
2026-03-11 03:44:24 +0000

    Set up clear_screen and started work on number_features

--- fun_with_numbers.py
+++ fun_with_numbers.py
@@ -1,10 +1,16 @@
 """Recreation of the original Fun with numbers program"""
 #Developed by Jeyson Blain :P
 
+#Imports required libraries
+import os
+
 def main():
     """The main menu"""
+    #Sets exit condition to false
     exit_flag = False
+
     while not exit_flag:
+        clear_screen()
         print('Welcome to Fun With Numbers')
         print('Choose from the menu below:')
         print(' (A) Check number features')
@@ -13,28 +19,45 @@ def main():
         print('\n (X) Save and exit')
         choice = input('Choice: ').upper()
     
+    #Takes user input and opens sub-routines
         if choice == 'A':
             number_features()
             input()
-            pass
         elif choice == 'B':
-            pass
+            plotter()
         elif choice == 'C':
-            pass
+            stats()
         elif choice == 'X':
             exit_flag = True
 
+#
 def number_features():
     """Displays features of a number"""
-    print("Number features routine")
+    clear_screen()
+    #print("Number features routine")
+    number = int(input("Please enter a whole number that can be checked over: "))
+    print(f"The features of {number} are...")
+    if number > 0:
+        print(" Positive")
+    elif number < 0:
+        print(" Negative")
+    else:
+        print(" Zero")
     input()
+
 def plotter():
     """Plot x and y cords on a table"""
+    clear_screen()
     print("Plotter routine")
     input()
 def stats():
     """Show statistics about numbers used in app"""
+    clear_screen()
     print("Statistics routine")
     input()
+
+def clear_screen():
+    """Clears the screen"""
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 main()
