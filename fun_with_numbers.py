3863cf9d69b691b5a831f1780dd77806e237fae7
notjeyson999 <jeyson.blain625@education.nsw.gov.au>
2026-03-12 22:56:06 +0000

    definded draw_graph list factors determine prime number

--- fun_with_numbers.py
+++ fun_with_numbers.py
@@ -29,21 +29,47 @@ def main():
             stats()
         elif choice == 'X':
             exit_flag = True
-
 #
 def number_features():
     """Displays features of a number"""
     clear_screen()
     #print("Number features routine")
     number = int(input("Please enter a whole number that can be checked over: "))
-    print(f"The features of {number} are...")
+    print(f"\nThe features of {number} are...")
     if number > 0:
         print(" Positive")
     elif number < 0:
         print(" Negative")
     else:
         print(" Zero")
+
+    #Check if number is odd or even
+    if number % 2 == 0:
+        print(" Even")
+    else:
+        print(" Odd")
+    
+
+    #List all factors of the number
+    print(" Factors are", end="")
+    factor_count = 0
+    for i in range(1, number + 1):
+        if number % i == 0:
+            print(" " + str(i), end="")
+            factor_count += 1
+
+    #Check if number is a prime
+    if factor_count == 2:
+        print("\an Is a prime number")
+    else:
+        print("\n Is not a prime number")
     input()
+
+def draw_graph():
+    """Draws the graph for the plotter routine"""
+    print("                                                       x axis")
+    print("      1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38")
+    print("   --------------------------------------------------------------------------------------------------------------------")
 
 def plotter():
     """Plot x and y cords on a table"""
