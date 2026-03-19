f93db504c2f4867ecf95b2c8193b3caa9259f9b9
notjeyson999 <jeyson.blain625@education.nsw.gov.au>
2026-03-19 22:54:44 +0000

    finish stats and began working on save and exit

--- fun_with_numbers.py
+++ fun_with_numbers.py
@@ -1,56 +1,64 @@
-"""Recreation of the original Fun with numbers program"""
-#Developed by Jeyson Blain :P
+'''Recreation of the original Fun with Numbers''' 
+#Devoloped by Jeyson Blain :P
 
 #Imports required libraries
 import os
 
+#State global variables
+NUMBER_COUNT = 0
+NUMBER_TOTAL = 0
+SMALLEST_NUMBER = 0
+LARGEST_NUMBER = 0
+PLOT_COUNT = 0
+
+
 def main():
-    """The main menu"""
-    #Sets exit condition to false
+    '''The main menu'''
+    #Sets the exit condition to false
     exit_flag = False
 
     while not exit_flag:
         clear_screen()
-        print('Welcome to Fun With Numbers')
-        print('Choose from the menu below:')
-        print(' (A) Check number features')
-        print(' (B) Plot numbers')
-        print(' (C) Check overall stats')
-        print('\n (X) Save and exit')
-        choice = input('Choice: ').upper()
+        print("Welcome to the Fun with Numbers!")
+        print("Choose from the menu below:")
+        print(" (A) Check number features")
+        print(" (B) Plot numbers")
+        print(" (C) Check overall stats")
+        print("\n (X) Save and exit")
+        choice = input("Choice: ").upper()
 
-    #Takes user input and opens sub-routines
-        if choice == 'A':
+    #Takes user input and opens sub routines
+        if choice == "A":
             number_features()
-            input()
-        elif choice == 'B':
+        elif choice == "B":
             plotter()
-        elif choice == 'C':
+        elif choice == "C":
             stats()
-        elif choice == 'X':
+        elif choice == "X":
             exit_flag = True
-#
+
 def number_features():
     """Displays features of a number"""
     clear_screen()
-    #print("Number features routine")
+    global NUMBER_COUNT, NUMBER_TOTAL, SMALLEST_NUMBER, LARGEST_NUMBER
     number = int(input("Please enter a whole number that can be checked over: "))
-    print(f"\nThe features of {number} are...")
-    if number > 0:
+    print(f"The features of {number} are...")
+
+    #Check if the number is odd or even
+    if number < 0:
         print(" Positive")
     elif number < 0:
         print(" Negative")
     else:
         print(" Zero")
 
-    #Check if number is odd or even
+    #Check if the number is odd or even
     if number % 2 == 0:
         print(" Even")
     else:
         print(" Odd")
 
-
-    #List all factors of the number
+    #Lists all the factors of te number
     print(" Factors are", end="")
     factor_count = 0
     for i in range(1, number + 1):
@@ -65,31 +73,77 @@ def number_features():
         print("\n Is not a prime number")
     input()
 
-def draw_graph():
+    #Update global variables
+    if NUMBER_COUNT == 0:
+        SMALLEST_NUMBER == number
+        LARGEST_NUMBER == number
+    else:
+        SMALLEST_NUMBER = min(number, SMALLEST_NUMBER)
+        LARGEST_NUMBER = max(number, LARGEST_NUMBER)
+
+    NUMBER_COUNT += 1
+    NUMBER_TOTAL += number
+
+def draw_graph(table):
     """Draws the graph for the plotter routine"""
-    print("                                                       x axis")
-    print("      1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38")
-    print("   --------------------------------------------------------------------------------------------------------------------")
-    print("   --------------------------------------------------------------------------------------------------------------------")
+    print("                                                     x axis")
+    print("     1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38")
+    print("  --------------------------------------------------------------------------------------------------------------------")
+    for i, row in enumerate(table, start=1):
+        print(f"{i:2}|  {'  '.join(row)} |")
+    print("  --------------------------------------------------------------------------------------------------------------------")
+
 
 def plotter():
     """Plot x and y cords on a table"""
     clear_screen()
-    print("Plotter routine")
-    input()
+    #Creates a blank table with specified rows and columns
+    global PLOT_COUNT
+    num_columns = 38
+    num_rows = 12
+    table = [[" " for column in range(num_columns)] for row in range(num_rows)]
+    draw_graph(table)
+    print("Enter a coordinate below to be added to the plot")
+    while True:
+        try:
+            x_axis = int(input("x axis: "))
+            if 0 < x_axis <= num_columns:
+                y_axis = int(input("y axis: "))
+                if 0 < y_axis <= num_columns:
+                    clear_screen()
+                    PLOT_COUNT += 1
+                    table[y_axis - 1] [x_axis - 1] = "x"
+                    if 0 < x_axis <= num_rows:
+                        draw_graph(table)
+                        another_cord = input("Do you wish to add another coordinate (y/n)? ").lower()
+                        if another_cord == 'n':
+                            break
+                else:
+                    pass
+            else:
+                pass
+        except ValueError:
+            print("Invalid input. Please use integers.")
+        input("press enter to go back to the main menu.")
 
 def stats():
     """Show statistics about numbers used in app"""
     clear_screen()
-    #Creates a blank table with specified rows and columns#
-    num_columns = 38
-    num_rows = 12
-    table = [[" " for column in range(num_columns)] for row in range(num_rows)]
-    print("Statistics routine")
+    print("Here are your statistics of overall use:")
+    print(f" Numbers entered:  {NUMBER_COUNT}")
+    print(f" Total of numbers:  {NUMBER_TOTAL}")
+    print(f" Average of Numbers:  {NUMBER_TOTAL} / {NUMBER_COUNT}")
+    print(f" Smallest number entered:  {SMALLEST_NUMBER}")
+    print(f" Largest Number entered:  {PLOT_COUNT}")
     input()
 
 def clear_screen():
     """Clears the screen"""
     os.system('cls' if os.name == 'nt' else 'clear')
+
+def save_stats():
+    """Saves statistics from current session to file"""
+    with open('stats/stats.txt', 'w', encoding='utf8') as f:
+        
 
 main()
