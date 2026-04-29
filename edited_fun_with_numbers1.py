bca5ff7241bc9fd7495a38852884cac8a0dbdaba
notjeyson999 <jeyson.blain625@education.nsw.gov.au>
2026-04-29 08:46:23 +0000

    Changed font colours up until higher or lower game

--- edited_fun_with_numbers1.py
+++ edited_fun_with_numbers1.py
@@ -9,11 +9,11 @@ import time
 import random
 
 #States Colours for UI
-RESET   = '\033[0m'
-RED     = '\033[31m'
-GREEN   = '\033[32m'
-YELLOW  = '\033[33m'
-CYAN    = '\033[36m'
+RESET   = '\033[0m'  #Sets to default#
+RED     = '\033[31m' #Special Characters#
+GREEN   = '\033[32m' #Choices#
+YELLOW  = '\033[33m' #Text#
+CYAN    = '\033[36m' #Input#
 
 #State global variables
 NUMBER_COUNT = 0
@@ -40,7 +40,7 @@ def main():
         print(f" {GREEN}(C){RESET} {YELLOW}Higher or Lower")
         print(f" {GREEN}(D){RESET} {YELLOW}Check overall stats")
         print(f"\n {GREEN}(X){RESET} {YELLOW}Save and exit")
-        choice = input(f"{CYAN}Choice: {RESET}").upper()
+        choice = input(f"{GREEN}Choice: {CYAN}").upper()
 
     #Takes user input and opens sub routines
         if choice == "A":
@@ -53,7 +53,7 @@ def main():
             stats()
         elif choice == "X":
             save_stats()
-            print("Come again soon :D")
+            print(f"{YELLOW}Come again soon :D")
             time.sleep(0.25)
             exit_flag = True
 
@@ -64,30 +64,30 @@ def number_features():
         clear_screen()
         global NUMBER_COUNT, NUMBER_TOTAL, SMALLEST_NUMBER, LARGEST_NUMBER
         try:
-            number = int(input("Please enter a whole number that can be checked over: "))
+            number = int(input(f"{GREEN}Please enter a whole number that can be checked over: {CYAN}"))
             if number == int:
-                print(f"The features of {number} are...")
+                print(f"{YELLOW}The features of {number} are...")
         except ValueError:
-            print("Please input an integer")
-            print("Press enter to return to main menu")
+            print(F"{YELLOW}Please input an integer")
+            print(F"{YELLOW}Press enter to return to main menu")
             break
 
         #Check if the number is odd or even
         if number > 0:
-            print(" Positive")
+            print(F" {YELLOW}Positive")
         elif number < 0:
-            print(" Negative")
+            print(F" {YELLOW}Negative")
         else:
-            print(" Zero")
+            print(F" {YELLOW}Zero")
 
         #Check if the number is odd or even
         if number % 2 == 0:
-            print(" Even")
+            print(F" {YELLOW}Even")
         else:
-            print(" Odd")
+            print(F"{YELLOW} Odd")
 
         #Lists all the factors of the number
-        print(" Factors are", end="")
+        print(F" Factors are{RED}", end="")
         factor_count = 0
         for i in range(1, number + 1):
             if number % i == 0:
@@ -96,14 +96,14 @@ def number_features():
 
         #Check if number is a prime
         if factor_count == 2:
-            print("\n Is a prime number")
+            print(F"\n {YELLOW}Is a prime number")
         else:
-            print("\n Is not a prime number")
+            print(F"\n {YELLOW}Is not a prime number")
         
         #Displays number as Binary, Hexadecimal and Octal
-        print(f" Binary: {bin(number)}")
-        print(f" Hexadecimal: {hex(number)}")
-        print(f" Octal: {oct(number)}")
+        print(f" {YELLOW}Binary: {RED}{bin(number)}{YELLOW}")
+        print(f" {YELLOW}Hexadecimal: {RED}{hex(number)}{YELLOW}")
+        print(f" {YELLOW}Octal: {RED}{oct(number)}{YELLOW}")
 
         #Update global variables
         if NUMBER_COUNT == 0:
@@ -117,21 +117,21 @@ def number_features():
         NUMBER_TOTAL += number
 
         #Asks user if they want to check another number
-        again = input("Do you want to check another number (y/n)?: ").lower()
+        again = input(F"{GREEN}Do you want to check another number (y/n)?: {CYAN}").lower()
         if again != "y":
-            print("Press Enter to return to main menu")
+            print(F"{YELLOW}Press Enter to return to main menu")
             break
     input()
 
 
 def draw_graph(table):
     """Draws the graph for the plotter routine"""
-    print("                                                     x axis")
-    print("     1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38")
-    print("  --------------------------------------------------------------------------------------------------------------------")
+    print(F"{RED}                                                     x axis")
+    print(F"{RED}     1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38")
+    print(F"{RED}  --------------------------------------------------------------------------------------------------------------------")
     for i, row in enumerate(table, start=1):
         print(f"{i:2}|  {'  '.join(row)} |")
-    print("  --------------------------------------------------------------------------------------------------------------------")
+    print(F"{RED}  --------------------------------------------------------------------------------------------------------------------")
 
 
 def plotter():
@@ -143,19 +143,19 @@ def plotter():
     num_rows = 12
     table = [[" " for column in range(num_columns)] for row in range(num_rows)]
     draw_graph(table)
-    print("Enter a coordinate below to be added to the plot")
+    print(F"{GREEN}Enter a coordinate below to be added to the plot")
     while True:
         try:
-            x_axis = int(input("x axis: "))
+            x_axis = int(input(F"{GREEN}x axis: {CYAN}"))
             if 0 < x_axis <= num_columns:
-                y_axis = int(input("y axis: "))
+                y_axis = int(input(F"{GREEN}y axis: {CYAN}"))
                 if 0 < y_axis <= num_columns:
                     clear_screen()
                     PLOT_COUNT += 1
-                    table[y_axis - 1] [x_axis - 1] = "x"
+                    table[y_axis - 1] [x_axis - 1] = F"{GREEN}x{RED}"
                     if 0 < x_axis <= num_rows:
                         draw_graph(table)
-                        another_cord = input("Do you wish to add another coordinate (y/n)? ").lower()
+                        another_cord = input(F"{GREEN}Do you wish to add another coordinate (y/n)? {CYAN}").lower()
                         if another_cord != 'y':
                             break
                 elif y_axis > 12:
@@ -163,63 +163,63 @@ def plotter():
             elif x_axis > 38:
                 break
         except ValueError:
-            print("Invalid input. Please use integers.")
+            print(F"{YELLOW}Invalid input. Please use integers.")
 
 def higher_lower():
     """Higher-or-Lower game"""
     clear_screen()
     global TOTAL_GUESSES, CORRECT_GUESSES, TOTAL_INVALID_GUESSES
-    print("Hello and welcome to Higher or Lower!!!")
+    print(F"{YELLOW}Hello and welcome to Higher or Lower!!!")
     time.sleep(0.5)
     print("A game where you have to guess the magic number between 1-10")
     time.sleep(0.5)
     print("But be wary, for if you shall exceed 5 attempts, you will be out!")
     time.sleep(0.5)
-    print("Good luck player!!!")
+    print(F"Good luck player!!!{RESET}")
     time.sleep(0.5)
 
     num = random.randint(1, 10)
     try:
         count = 1
         while count < 6:
-            guess = int(input("What do you think it is?: "))
+            guess = int(input(F"{GREEN}What do you think it is?: {CYAN}"))
             if guess == num:
-                print("BAZINGA! You are correct!")
-                print(f"You guessed it in {count} guesses")
-                print("Press enter to return to main menu")
+                print(F"{YELLOW}BAZINGA! You are correct!")
+                print(f"You guessed it in {RED}{count}{YELLOW} guesses")
+                print(F"Press enter to return to main menu{RESET}")
                 CORRECT_GUESSES += 1
                 TOTAL_GUESSES += 1
                 break
             elif guess == 67:
-                print(f"89. {count}/5")
+                print(f"{RED}89. {count}/5")
                 time.sleep(0.67)
                 TOTAL_GUESSES += 1
                 count += 1
             elif guess > 10:
-                print("Please enter a number between 1-10!")
+                print(F"{YELLOW}Please enter a number between 1-10!")
                 time.sleep(0.5)
                 TOTAL_INVALID_GUESSES += 1
             elif guess < 1:
-                print("Please enter a number between 1-10!")
+                print(F"{YELLOW}Please enter a number between 1-10!")
                 time.sleep(0.5)
                 TOTAL_INVALID_GUESSES += 1
             elif guess > num:
-                print(f"WRONG! Lower. {count}/5")
+                print(f"{YELLOW}WRONG! Lower. {RED}{count}/5{RESET}")
                 TOTAL_GUESSES += 1
                 count += 1
             elif guess < num:
-                print(f"WRONG! Higher. {count}/5")
+                print(f"{YELLOW}WRONG! Higher. {RED}{count}/5{RESET}")
                 TOTAL_GUESSES += 1
                 count += 1
         else:
-            print("Oh No! You ran out of attempts.")
-            print(f"The number we were looking for was {num}")
-            print("Press enter to return to the main menu")
+            print(F"{YELLOW}Oh No! You ran out of attempts.")
+            print(f"{YELLOW}The number we were looking for was {RED}{num}")
+            print(F"{YELLOW}Press enter to return to the main menu")
     except ValueError:
-        print("Invalid input! Please input an integer")
+        print(F"{YELLOW}Invalid input! Please input an integer")
         TOTAL_INVALID_GUESSES += 1
         time.sleep(0.5)
-        print("Press Enter to return to menu")
+        print(f"Press Enter to return to menu{RESET}")
     input()
 
 def stats():
